6fc726ef9c5b9d2d4ff049a1bd40b77b1d5f51e2
xuke <ke.xu@mtime.com>
2016-06-21 19:04:56 +0800

    promise练习
    Signed-off-by: xuke <ke.xu@mtime.com>

--- 进击nodejs基础(一)/http/http.js
+++ 进击nodejs基础(一)/http/http.js
@@ -3,10 +3,12 @@
 
 var http = require('http');
 
-http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	res.write('Hello Nodejs');
-	res.end();
-});
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('Hello Nodejs');
+    res.end();
+  })
+  .listen(2016);
